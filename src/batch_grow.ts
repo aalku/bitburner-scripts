cb7d9a558b6dbc02f3ef7f85ba402bfb67d1d3dd
aalku <aalku7@gmail.com>
2023-05-22 00:21:17 +0200

    working on hackingController

--- src/batch_grow.ts
+++ src/batch_grow.ts
@@ -2,5 +2,15 @@ import { NS } from "@ns";
 
 /** @param {NS} ns */
 export async function main(ns: NS) {
-    // TODO
+  const target = ns.args[0] as string;
+  const start = ns.args[1] as number;
+  const end = ns.args[2] as number;
+  const delay = start - Date.now();
+  if (delay < 0) {
+    ns.print(`ERROR: ${-delay} ms too late.`);
+    return;
+  }
+  ns.print(`delay to start growing is ${delay} ms`);
+  await ns.grow(target, { additionalMsec: delay });
+  ns.print(`Finished with a delay of ${Date.now() - end}`);
 }
